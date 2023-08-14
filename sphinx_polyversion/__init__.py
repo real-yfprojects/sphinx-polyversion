25fd6243285b5ae4a5ffc15002da7830d3dff635
real-yfprojects <62463991+real-yfprojects@users.noreply.github.com>
2023-08-14 19:13:12 +0200

    Fix `__all__` tuples.
    * sphinx_polyversion/__init__.py : Add `__all__` with items from `driver` and `api`.
    
    * sphinx_polyversion/git.py : Add `refs_by_type`.

--- sphinx_polyversion/__init__.py
+++ sphinx_polyversion/__init__.py
@@ -9,8 +9,18 @@ logger: Logger
 """
 from logging import DEBUG, NullHandler, getLogger
 
-from sphinx_polyversion.api import *  # noqa: F403
-from sphinx_polyversion.driver import *  # noqa: F403
+from sphinx_polyversion.api import apply_overrides, load, order_versions
+from sphinx_polyversion.driver import DefaultDriver
+from sphinx_polyversion.json import GLOBAL_DECODER, GLOBAL_ENCODER
+
+__all__ = (
+    "apply_overrides",
+    "load",
+    "order_versions",
+    "DefaultDriver",
+    "GLOBAL_DECODER",
+    "GLOBAL_ENCODER",
+)
 
 logger = getLogger(__name__)
 logger.setLevel(DEBUG)
